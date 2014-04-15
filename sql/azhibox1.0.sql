9452f835eaf36629f3ac87de0c17113905a80c18
iamazhi <chencongzhi520@gmail.com>
2014-04-15 21:52:02 +0800

    + add ab_comment.

--- sql/azhibox1.0.sql
+++ sql/azhibox1.0.sql
@@ -8,3 +8,13 @@ CREATE TABLE `ab_note` (
    `views` mediumint(11) unsigned NOT NULL default '0',
    PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ab_comment` (
+  `id` mediumint(11) unsigned NOT NULL auto_increment,
+  `targetType` varchar(30) NOT NULL,
+  `targetID` mediumint(11) NOT NULL,
+  `content` longblob NOT NULL,
+  `author` varchar(64) NOT NULL,
+  `createTime` datetime NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
