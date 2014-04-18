60dab1deb3f95c49e180be9a3079e4b47feb2b28
iamazhi <chencongzhi520@gmail.com>
2014-04-18 17:55:11 +0800

    * add fixed for footer bg and bamboo-leaf.

--- Home/Tpl/default/Public/footer.html.php
+++ Home/Tpl/default/Public/footer.html.php
@@ -1,6 +1,6 @@
 <div id="bambooForest"></div>
 <style>
-#bambooForest {width:100%; background:url("/Public/theme/default/images/bamboo-forest.png") repeat-x; height:250px;}
+#bambooForest {width:100%; background:url("/Public/theme/default/images/bamboo-forest.png") repeat-x; height:250px; position: fixed; bottom: 0px;}
 </style>
 
   </body>
